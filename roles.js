6c892f4f6f08b8423f4173caf758be9986aa267b
Mark Diez <markediez@gmail.com>
2016-05-04 15:19:20 -0700

    Add opening panel functionality

--- roles.js
+++ roles.js
@@ -1,9 +1,24 @@
-function openRoles() {
-  document.getElementsByClassName("col-md-4 app-panel background-gray")[0].style.display = "block";
-  document.getElementsByClassName("panel-wrapper")[0].className = "panel-wrapper background-gray";
+function openRoles(e) {
+  // Open Roles and change the background color
+  $('.roles').show();
+  $('.panel-wrapper').removeClass('background-blue');
+  $('.panel-wrapper').addClass('background-gray');
+
+  // Remove selected class from previously selected item
+  $("#application.app #sub-name-list .selected").removeClass("selected");
+
+  // Add selected class to the clicked button
+  $(e).addClass("selected");
 }
 
-function openAssignment() {
-  document.getElementsByClassName("col-md-4 app-panel background-white")[0].style.display = "block";
-  document.getElementsByClassName("panel-wrapper")[0].className = "panel-wrapper background-white";
+function openAssignment(e) {
+  $('.assign').show();
+  $('.panel-wrapper').removeClass('background-gray');
+  $('.panel-wrapper').addClass('background-white');
+
+  // Remove selected class from previously selected item
+  $("#application.roles #sub-name-list .selected").removeClass("selected");
+
+  // Add selected class to the clicked button
+  $(e).addClass("selected");
 }

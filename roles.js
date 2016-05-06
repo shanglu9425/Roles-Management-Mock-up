b4579a5f5ab27f2acf7a5df33a34d5b0a8200828
Mark Diez <markediez@gmail.com>
2016-05-06 15:16:04 -0700

    Control panel changes

--- roles.js
+++ roles.js
@@ -1,4 +1,4 @@
-function openRoles(e) {
+function openRoles(e, application) {
   // Open Roles and change the background color
   $('.roles').show();
   $('.panel-wrapper').removeClass('background-blue');
@@ -8,20 +8,29 @@ function openRoles(e) {
   $("#application.app #sub-name-list .selected").removeClass("selected");
   // Hide Assigned
   $('.assign').hide();
+  // remove show from previous application
+  $('.control-panel .container .show').removeClass('show');
 
 
   // Add selected class to the clicked button
   $(e).addClass("selected");
+  // Open application
+  // $(application).addClass('show');
+  $(application).addClass('show');
 }
 
-function openAssignment(e) {
+function openAssignment(e, application) {
   $('.assign').show();
   $('.panel-wrapper').removeClass('background-gray');
   $('.panel-wrapper').addClass('background-white');
 
   // Remove selected class from previously selected item
   $("#application.roles #sub-name-list .selected").removeClass("selected");
+  // Remove show from previous application
+  $('.control-panel .container .show').removeClass('show');
 
   // Add selected class to the clicked button
   $(e).addClass("selected");
+  // Open application
+  $(application).addClass('show');
 }
